c30ea2b710d60ea164e4b2472d1597bf3915c6cf
valeskamdev <valeskkitty@gmail.com>
2022-11-16 14:03:28 -0300

    debugando

---
+++ src/aula/debugando/Main.java
@@ -0,0 +1,24 @@
+package aula.debugando;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        double largura = scanner.nextDouble();
+        double comprimento = scanner.nextDouble();
+        double metroQuadrado = scanner.nextDouble();
+
+        double area = largura * comprimento;
+        double preco = area * metroQuadrado;
+
+        System.out.printf("AREA = %.2f%n", area);
+        System.out.printf("PRECO = %.2f%n", preco);
+
+        scanner.close();
+    }
+}
