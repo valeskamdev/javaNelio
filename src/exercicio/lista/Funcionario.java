e7803edf77da3781926ce34b90b05d5fcc8bb510
valeskamdev <valeskkitty@gmail.com>
2022-11-23 17:27:54 -0300

    Exercicio do Funcionario completo (List)

---
+++ src/exercicio/lista/Funcionario.java
@@ -0,0 +1,54 @@
+package exercicio.lista;
+
+public class Funcionario {
+
+    private int id;
+    private String name;
+    private double salary;
+
+    public Funcionario() {
+
+    }
+
+    public Funcionario(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public void increaseSalary(double percentage) {
+        salary += salary * percentage / 100;
+    }
+
+    public String toString() {
+        return id
+                + ", "
+                + name
+                + ", "
+                + String.format("%.2f", salary);
+    }
+}
---
+++ src/exercicio/lista/Main.java
@@ -0,0 +1,77 @@
+package exercicio.lista;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /* Fazer um programa para ler um número inteiro N e depois os dados (id, nome e salario)
+         de N funcionários. Não deve haver repetição de id. Em seguida, efetuar o aumento de X por
+         cento no salário de um determinado funcionário. Para isso, o programa deve ler um id e o
+         valor X. Se o id informado não existir, mostrar uma mensagem e abortar a operação. Ao final,
+         mostrar a listagem atualizada dos funcionários. Lembre-se de aplicar a técnica de encapsulamento
+         para não permitir que o salário possa ser mudado livremente. Um salário só pode ser aumentado
+         com base em uma operação de aumento por porcentagem dada. */
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        List<Funcionario> list = new ArrayList<>();
+
+        System.out.print("How many employees will be registered? ");
+        int count = scanner.nextInt();
+
+        for (int i = 0; i < count; i++) {
+            System.out.println("Employee #" + (i + 1));
+            System.out.print("Id: ");
+            int id = scanner.nextInt();
+
+            // verificando se o id já está na lista
+            while (hasId(list, id)) {
+                System.out.print("Id already taken. Try again: ");
+                id = scanner.nextInt();
+            }
+
+            System.out.print("Name: ");
+            scanner.nextLine();
+            String name = scanner.nextLine();
+
+            System.out.print("Salary: ");
+            double salary = scanner.nextDouble();
+            list.add(new Funcionario(id, name, salary));
+
+            System.out.println();
+        }
+
+        System.out.print("Enter the employee id that will have salary increase: ");
+        int id = scanner.nextInt();
+
+        // verificando se o id está na lista
+        Funcionario emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+        if (emp == null) {
+            System.out.println("This id does not exist!");
+        }
+        else {
+            System.out.print("Enter the percentage: ");
+            double percentage = scanner.nextDouble();
+            emp.increaseSalary(percentage);
+        }
+
+        System.out.println();
+        System.out.println("List of employees:");
+        for (Funcionario obj : list) {
+            System.out.println(obj);
+        }
+
+        scanner.close();
+    }
+
+    // verificando se a lista tiver o id do funcionario forncecido
+    public static boolean hasId(List<Funcionario> list, int id) {
+        Funcionario emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+        return emp != null;
+    }
+}
